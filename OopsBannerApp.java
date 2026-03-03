84a2f4a01a4842fe4b1138b4cd6683ab70e5cc83
sonal-a11y <os1378@srmist.edu.in>
2026-03-03 11:59:31 +0530

    added UC8

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -139,7 +139,7 @@
      }
 }*/
 
-public class OopsBannerApp {
+/*public class OopsBannerApp {
  static class CharacterPatternMap {
 	 private Character character;
 	 private String[] pattern;
@@ -186,5 +186,52 @@ public class OopsBannerApp {
 		String message = "OOPS";
 		printMessage(message,charMaps);
 	}
-}
+}*/
 
+public class OopsBannerApp {
+
+    private static final java.util.Map<Character, String[]> patterns =
+            new java.util.HashMap<>();
+
+    static {
+        patterns.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        patterns.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        patterns.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+    }
+
+    public static void render(String word) {
+
+        int height = patterns.get('O').length;
+
+        for (int i = 0; i < height; i++) {
+            for (char ch : word.toCharArray()) {
+                System.out.print(patterns.get(ch)[i] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        render("OOPS");
+    }
+}
